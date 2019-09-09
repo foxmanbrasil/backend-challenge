9da523271e5c1804f89298794eb764d060bbc410
Elhamy Gabriel Girgis <foxmanbrasil@gmail.com>
2019-09-09 07:52:55 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     Google: {
         provider: 'google',
         httpAdapter: 'https', // Default
-        apiKey: 'AIzaSyDyBAVO9U92gWkT8Jb_unZBahycwpwjyIw', // mine is not working any more i am using AMAZON , not google cloud // You must enable Billing on the Google Cloud Project 
+        apiKey: 'xxxxxxxxxxxxxx', // mine is not working any more i am using AMAZON , not google cloud // You must enable Billing on the Google Cloud Project 
         formatter: null // 'gpx', 'string'.
     }
 };
